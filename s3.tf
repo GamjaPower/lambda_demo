119cac8ad8912bf9a0ddfe0260a910f1c7702141
GamjaPower <sst9696@gmail.com>
2021-08-20 15:40:25 +0900

    change version location

--- s3.tf
+++ s3.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = ">=3.0"
     }
   }
   backend "remote" {
@@ -13,8 +14,9 @@ terraform {
   }
 }
 
+
 provider "aws" {
-  version = ">=3.0"
+  
   region  = "ap-northeast-2"
 }
 
